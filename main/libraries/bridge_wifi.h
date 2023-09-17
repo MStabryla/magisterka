9e2c807cbc40b682a25bdf9d752c3ff26fbc9ad3
Mateusz Stabryła <mstabryla@op.pl>
2023-09-17 14:01:44 +0200

    removing nat solving code

--- main/libraries/bridge_wifi.h
+++
@@ -1,6 +0,0 @@
-#include "bridge_wifi.c"
-
-esp_err_t esp_bridge_wifi_set(wifi_mode_t mode,
-                              const char *ssid,
-                              const char *password,
-                              const char *bssid);
